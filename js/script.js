9f86f6c9ef3d86eed5582c8de8cbb17fc1336fe1
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-25 11:18:54 -0400

    more about variables

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
 
+// The variables must be written in camelCase.
 
-/* 
+/*
+***RULES***
 
-Multiline 
+1 - You can not start the name of variable with digits
+2 - You can not use as the name keywords
+3 - do not use   
+3 - do not use spaces in names
+4 - you should not use operators in the names  
 
-comments 
+***TYPES***
+
+1 - Number
+2 - Strings
+3 - Boolean
+4 - Arrays and Objects
+5 - Empty
+6 - Undefined
+7 - Functions
 
 */
 
-// one-line comments
 
-a = 10;
-b = 5;
 
-alert(a + b);
+var shoes;
+var pants = 50;
+var tShirt = 20;
+var swimSuit;
+
+shoes = 30 * 3;
+tShirt= 26;
+swimSuit= 15 + 3;
+
+var total =shoes + pants +  tShirt + swimSuit
+
+alert('total price: $' + total);
+
+var myName = 'David';
+console.log(myName);
