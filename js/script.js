56d551f838d8442e988b62c633d4940075dbdf02
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-26 09:32:32 -0400

    more about arrays

--- js/script.js
+++ js/script.js
@@ -5,45 +5,55 @@ the item in the array and it starts at zero.
 
 array = [item1, item2, ..., itemN];
 
-get an item by index....
-array[n] = [itemN];
-
-length of the array...
-array.length
-
-add items in the array...
-array[array.length] = newItem
-array.push('newItem')
-
-replacing items in the array...
-array[i] = itemI, so array[i] = itemZ
-
-KeyValues array...
-
-array = [];
-array[key1] = value1;
-array[key2] = value2;
-.
-.
-.
-array[keyN] = valueN;
-
+1 - join is used to join elements by sent argument
+2 - concat is used for concatenate two arrays
+3 - pop is used for removing the last element and show it
+4 - push is used for add an element at the end of array
+5 - shift is used for remove the first array element
+6 - unshift is used for add an element at the begin of array
+7 - sort is used for sort alphabetically or increasingly the array elements
+8 - reverse is used for reserve the order of the array
+9 - slice is used for slice the array in the form [n, m)
+10 - splice is used from wich place we want removing or replacing elements 
 */
 
 var supermarket = ['milk', 'coffee', 'bread', 'cheese', 'ham'];
 
-console.log(supermarket[3]); // cheese!
+prices = [2, 3, 10, 5, 8];
 
-supermarket[supermarket.length] = 'apple';
-supermarket.push('banana');
+function myFunction(){
+    var demo = document.getElementById('demo');
+    demo.innerHTML = supermarket.join('<br>');
+}
+
+console.log(supermarket.concat(prices));
+
+console.log(supermarket.pop(), supermarket);
+
+supermarket.push('ham');
 console.log(supermarket);
 
-supermarket[2] = 'grape';
+supermarket.shift();
 console.log(supermarket);
 
-person = [];
-person['name'] = 'David';
-person['surname'] = 'Sotto Mayor';
-person['age'] = 20;
-console.log(person);
+supermarket.unshift('milk');
+console.log(supermarket);
+
+supermarket.sort();
+prices.sort();
+console.log(supermarket, prices);
+
+supermarket.reverse();
+prices.reverse();
+console.log(supermarket, prices);
+
+console.log(supermarket.slice(0, 3));
+
+
+prices.splice(0, 2, 'replacing', 'replacing','replacing');
+
+console.log(prices);
+
+
+
 
