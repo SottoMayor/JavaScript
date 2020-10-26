c1bda8f84f0ed5aa4cba09a1f97c09e5b4f311b8
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-26 11:40:23 -0400

    Adding and removing elements from DOM

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 /*
 
-querySelector() - selecting the first occurrence of query and returning it as single variable
+How to add and remove DOM elements
 
-querySelectorAll() - selecting all possible occurrences of query and returning the array 
+createElement('nameOfTag')
+
+appendChild
+
+removeChild
+
+nameOfVar.parentNode.removeChild(nameOfVar)
 
 */
 
-var li = document.querySelectorAll('li');
+var doc = document.querySelector('body');
 
-li[5].style.color = 'orange';
+var p = document.createElement('p');
+p.style.color = 'green';
+p.className = 'myP';
+p.innerHTML = 'My Text Here';
+p.style.fontSize = '30px'
 
-liTest = document.querySelectorAll('#test li');
+doc.appendChild(p);
 
-for (var index in liTest){
-    li[index].style.color = 'red';
-}
+//to remove...
 
+//this form
+var removeList0 = document.querySelector('#test0').remove();
+
+//or this form
+var removeList = document.querySelector('#test');
+
+removeList.parentNode.removeChild(removeList);
