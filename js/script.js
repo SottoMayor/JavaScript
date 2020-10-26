f5056ba65f68684d12a03df3f1534441c53c792c
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-26 10:02:16 -0400

    for-in loop

--- js/script.js
+++ js/script.js
@@ -2,35 +2,25 @@
 
 Loops are used when we would like to perform the same task several times.
 
-break and continue...
+for-in
 
-break is used for stop the iteration
-continue is used for filter the iteration
+for (var nameOfVariable in array){
+    expression...
+}
+
+In this loop, the iterations has the same lenght that the array or object.
 
 */
 
 
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-for(i = 0; i < numbers.length; i++){
-    if ( numbers[i] % 2 == 0 ){
-        continue
-    }else{
-        console.log(numbers[i] + ' is odd!')   
-    }
-
-    //here an array of odd numbers! because we used the 'continue' for filter the even numbers!
+var person = {
+    name : 'David',
+    surname : 'Sotto Mayor',
+    age : '20'
 }
 
-
-for(i = 0; i < numbers.length; i++){
-    if ( numbers[i] == 5 ){
-        break
-    }else{
-        console.log(numbers[i])   
-    }
-
-    //When the position number is five, the iteration is stoped!
-
+for (var atributes in person){
+    console.log(atributes);
+    console.log(person[atributes]);
 }
