a7a7a6f2e02fbb4e55772ff3f7f68ec0cc8d6f5b
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-26 09:53:49 -0400

    break and continue

--- js/script.js
+++ js/script.js
@@ -2,25 +2,10 @@
 
 Loops are used when we would like to perform the same task several times.
 
-While....
+break and continue...
 
-cont = 0
-
-while(stop condition){
-    expression...
-
-    cont expression
-}
-
-While looping through arrays...
-
-for(cont = number; stop condition; cont incrementation){
-    .
-    .
-    expression
-    .
-    .
-}
+break is used for stop the iteration
+continue is used for filter the iteration
 
 */
 
@@ -30,10 +15,22 @@ var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 for(i = 0; i < numbers.length; i++){
     if ( numbers[i] % 2 == 0 ){
-        console.log(numbers[i] + ' is even!')
+        continue
     }else{
         console.log(numbers[i] + ' is odd!')   
     }
+
+    //here an array of odd numbers! because we used the 'continue' for filter the even numbers!
 }
 
 
+for(i = 0; i < numbers.length; i++){
+    if ( numbers[i] == 5 ){
+        break
+    }else{
+        console.log(numbers[i])   
+    }
+
+    //When the position number is five, the iteration is stoped!
+
+}
