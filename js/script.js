ec494fc7ed9971f8d49427d701f1f1b56147a6d0
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-28 12:10:52 -0400

    how to write quotes with strings

--- js/script.js
+++ js/script.js
@@ -1,45 +1,17 @@
 /*
 
-Strings!
+quote and strings definition!
 
-charAt(index) - what caracter is at index
-toUpperCase - write the caracters in UPPERCASE form
-substring or slice(from, to) - slice the string 
-substr(from, howMany) - get a substring of the string
-split - splitting string into the array
-join - connect an array into a string
-replace(whatToReplace, forWhat) - replacing content of string 
-trim - remove spaces into string
-lastIndexOf('value') - the last place where the 'value' is in string
-indexOf - the first place where the 'value' is in string
+obs: " != \" or ' != \' 
 
 */
 
 window.onload = function(){
 
     var info = document.getElementById('info');
-    var tmp = 'david';
+    var tmp = 'I\'m ok, and I said "Hello! Mr Gordon..."' ;
     
+    info.innerHTML = tmp;
 
-    // david -> David
-    info.innerHTML = tmp.charAt(0).toUpperCase() + tmp.substr(1,);
-
-
-    var link = 'https://google.com';
-
-    console.log(link.lastIndexOf('/'));;
-    console.log(link.indexOf('/'));
-
-    myName = 'David Sotto Mayor Maciel Fernandes';
-
-    arrayName = myName.split(' ')
-    console.log(arrayName);
-
-    comanName = arrayName.join(', ');
-    console.log(comanName);
-
-
-    var spaceeeName = '    David! '
-    console.log(spaceeeName.trim());
 }
 
