e7aea017dd658cb4cec73dc49da2289a81aeb64a
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-28 09:33:40 -0400

    stopPropagation

--- js/script.js
+++ js/script.js
@@ -1,50 +1,15 @@
-/* 
+/*
 
-addEventListener(...) 
-removeEventListener(...)
+Propagation of events (bubbling)
+
+var event = event || window.event;
+
+if(event.stopPropagation){
+    event.stopPropagation();
+}else{
+    event.cancelBubble = true;
+}
 
 */
 
-window.onload = function(){
 
-    var result = document.getElementById('result');
-    var stop = document.getElementById('stop');
-
-    /*
-    result.addEventListener('mouseenter', changeColor);
-    result.addEventListener('mouseleave', originalColor);
-    */
-
-    createEvent(result, 'mouseenter', changeColor);
-    createEvent(result, 'mouseleave', originalColor);
-    createEvent(result, 'click', orange);
-
-    
-    stop.addEventListener('click', function(){
-        result.removeEventListener('mouseenter', changeColor);
-        result.removeEventListener('mouseleave', originalColor);
-    })
-
-}
-
-function createEvent(obj, event, nameOfFunction){
-    if(addEventListener){
-        obj.addEventListener(event, nameOfFunction)
-    }
-}
-
-
-function changeColor(){
-    this.style.backgroundColor = '#444';
-    this.style.color = 'white';
-    this.style.transition= '0.7s';
-}
-
-function originalColor(){
-    this.style.backgroundColor = '#aaa';
-    this.style.color = 'black';
-}
-
-function orange(){
-    this.style.color = 'orange';
-}
