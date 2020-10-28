c0b2df8146e0856c28e6a34a9516e4fa7bebe3b7
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-28 09:48:34 -0400

    Prevent Default

--- js/script.js
+++ js/script.js
@@ -1,15 +1,49 @@
 /*
 
-Propagation of events (bubbling)
-
-var event = event || window.event;
-
-if(event.stopPropagation){
-    event.stopPropagation();
-}else{
-    event.cancelBubble = true;
-}
+Prevent Default... 
 
 */
 
+window.onload = function(){
 
+    var email = document.getElementById('email');
+
+    var btnSubmit = document.querySelector('#btn-submit');
+
+    btnSubmit.onclick = function(event){
+
+        var event = event || window.event;
+
+        if(event.preventDefault){
+            event.preventDefault()
+        }else{
+            event.returnValue = false;
+        }
+
+
+        var form = document.getElementById('form');
+
+        var tmp = document.getElementById('tmp');
+
+        if (email.value == 'Some valid Email...'){
+            form.submit();
+        }else{
+            tmp.innerHTML = 'The email is invalid!'
+        }
+
+        btnSubmit.oncontextmenu = function(event){
+
+            var event = event || window.event;
+
+            if(event.preventDefault){
+                event.preventDefault()
+            }else{
+                event.returnValue = false;
+            }
+
+        }
+
+    }
+
+
+}
