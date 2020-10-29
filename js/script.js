26f40412f922c90a24f3c2af70eb22c51537b08e
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-29 09:51:49 -0400

    Regular Expressions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 /*
+    REGEXP
+    regular expressions - formulas
 
-quote and strings definition!
+    stringToSearch.search(formula) - searches and return the index
+    stringToSearch.match(formula) - searches and returns an array
+    regExpVar.exec(stringToSearch) - same as above but it returns only one result after each execution
+    stringToSearch.replace(formula, "forWhat"); - replacing things in formula by "forWhat"
+    formula.test(stringToSearch); - testing if something from formula exists in stringToSearch
 
-obs: " != \" or ' != \' 
+    g - global - searching through full string
+    i - insensitive - case Insensitive 
+ */
 
-*/
+window.onload = function()
+{
+   var info = document.getElementById("info");   
 
-window.onload = function(){
+   var indexes = "A-565 B-12 K-51 A-53 A#58 S#45 A.51 a-98 a-4124 Aj244";
+   
+   var result = indexes.match(/A\.[0-9]+/gi);
+   
+   var telephoneNumber = "22 555 555 554";
+   
+   result = telephoneNumber.match(/^\d{2}\s[0-9]{3}\s[0-9]{3}\s[0-9]{3}$/);
+   
+   var testResult = (/^\d{2}\s[0-9]{3}\s[0-9]{3}\s[0-9]{3}$/).test(telephoneNumber);
+   
+   info.innerHTML = result;
+   
+   
+};
 
-    var info = document.getElementById('info');
-    var tmp = 'I\'m ok, and I said "Hello! Mr Gordon..."' ;
-    
-    info.innerHTML = tmp;
 
-}
+
+
+
 
