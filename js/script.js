286cd21a54cf4478c7fe05e7f5f1a67963be6f97
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-29 10:36:55 -0400

    JS Date

--- js/script.js
+++ js/script.js
@@ -1,39 +1,40 @@
 /*
-    REGEXP
-    regular expressions - formulas
 
-    stringToSearch.search(formula) - searches and return the index
-    stringToSearch.match(formula) - searches and returns an array
-    regExpVar.exec(stringToSearch) - same as above but it returns only one result after each execution
-    stringToSearch.replace(formula, "forWhat"); - replacing things in formula by "forWhat"
-    formula.test(stringToSearch); - testing if something from formula exists in stringToSearch
+    Manipulation of time using DATE object
 
-    g - global - searching through full string
-    i - insensitive - case Insensitive 
+    new Date()
+    new Date(miliseconds) - you can create Date typing how many miliseconds has passed from 1st January 1970 r
+    new Date(year, month, day, hours, minutes, seconds, milliseconds) - only first 3 arguments are required, 
+                                                                        we start counting months from ZERO!
+    new Date(dateString) where dateString is in format like:
+    np. YYYY-MM-DD, YYYY-MM, YYYY
+        YYYY/MM/DD, MM/DD/YYYY 
  */
+Date.prototype.getMonthInString = function()
+{
+   var months = ["January", "February", "March", "April", "May", "June", "July",
+                  "August", "September", "October", "November", "December"];
+   
+   return months[this.getMonth()];
+};
+Date.prototype.getFormattedDate = function()
+{
+ 
+};
+
+
 
 window.onload = function()
 {
-
-    var info = document.getElementById(info);
-
-    
-    var btn = document.getElementById('myForm').btn;
-
-    btn.onclick = function(event){
-
-        event.preventDefault;
-
-        var pw = document.getElementById('myForm').pw.value;
-        var formula = / (?=.*[a-z]) (?=.*[A-Z]) (?=.*[0-9]).{8, } /
-
-        info.innerHTML = formula.test(pw);
-    }
-
-    
-    
-
-   
+  var info = document.getElementById("info");   
+  
+  var today = new Date();
+  
+  //var tomorrow = new Date(today.getTime()+ 1000 * 60 * 60 * 24);
+  //var tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+  var tomorrow = new Date("2015-05-12");
+  info.innerHTML = tomorrow;
+          //today.getMonthInString() + "/" + today.getDate() + "/" + today.getFullYear();
 };
 
 
