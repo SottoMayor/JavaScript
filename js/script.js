2746604a6e72ebffc3909e15141847c3825aeb6b
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-29 10:25:31 -0400

    exercise

--- js/script.js
+++ js/script.js
@@ -14,20 +14,25 @@
 
 window.onload = function()
 {
-   var info = document.getElementById("info");   
 
-   var indexes = "A-565 B-12 K-51 A-53 A#58 S#45 A.51 a-98 a-4124 Aj244";
-   
-   var result = indexes.match(/A\.[0-9]+/gi);
-   
-   var telephoneNumber = "22 555 555 554";
-   
-   result = telephoneNumber.match(/^\d{2}\s[0-9]{3}\s[0-9]{3}\s[0-9]{3}$/);
-   
-   var testResult = (/^\d{2}\s[0-9]{3}\s[0-9]{3}\s[0-9]{3}$/).test(telephoneNumber);
-   
-   info.innerHTML = result;
-   
+    var info = document.getElementById(info);
+
+    
+    var btn = document.getElementById('myForm').btn;
+
+    btn.onclick = function(event){
+
+        event.preventDefault;
+
+        var pw = document.getElementById('myForm').pw.value;
+        var formula = / (?=.*[a-z]) (?=.*[A-Z]) (?=.*[0-9]).{8, } /
+
+        info.innerHTML = formula.test(pw);
+    }
+
+    
+    
+
    
 };
 
