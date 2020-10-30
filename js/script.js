8ad2faa2873ecaaae845c0844f8f415320612604
David Sotto Mayor <ddsmmf@gmail.com>
2020-10-30 11:00:05 -0400

    What are cookies?

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
-/*
+/*    Cookies - cookies are containers that can store
+                key=value1;key2=value2;
 
-location....
+              values like that are available to us even if the user leave our website
+              when the user visits your website again this value is still gonna be there
+              because of it we can identify the person visiting us
 
-*/ 
+              path=/
+              expires= - when the cookie expires (default when you close the session) (toUTCString() format)
+              max-age= - how old can the cookie be in seconds (not supported in ie6-8)
 
-window.onload = function() {
+              localstorage - for local              
+*/
 
-    var google = document.getElementById('google');
 
-    google.onclick = function(event){
+window.onload = function(){
 
-        event.preventDefault();
+  var createCookie = document.getElementById('createCookie');
+  var removeCookie = document.getElementById('removeCookie');
+  var info = document.getElementById('info');
 
-        window.location = this.getAttribute('href')
+  createCookie.onclick = function() {
 
-    }
+    document.cookie = 'name=David;max-age='+60*60*24+';path=/';
+    document.cookie = 'surname=Sotto Mayor;path=/';
+
+    var expirationDateOfCookie = new Date();
+    expirationDateOfCookie.setTime(expirationDateOfCookie.getTime() + 1000 * 60 * 60);
+
+  };
+
+  removeCookie.onclick = function() {
+
+    var expirationDateOfCookie = new Date();
+
+    document.cookie = 'surname=;expiress='+expirationDateOfCookie.getTime()+';path=/';
+
+  };
 
 };
+
+
+
+
+
+
+
